a9d1df90890fdb28722e22e9bf36fbce6aafcd72
BC-macair032 <bc-macair032@BC-macair032s-MacBook-Air.local>
2021-11-16 12:28:00 +0200

    added code to check if user clicking on correct circle and stopping game if 5 consecutive time user did not click on any circle.

--- src/App.js
+++ src/App.js
@@ -12,34 +12,62 @@ class App extends Component {
     score: 0,
     current: 0,
     gameOver: false,
+    pace: 1500,
+    rounds: 0,
+    didGameStart: false,
   };
   timer = undefined;
-  pace = 1500;
-  clickHandler = () => {
+
+  clickHandler = (id) => {
+    console.log("you clicked on", id);
+    if (this.state.current !== id) {
+      this.stopHandler();
+      return;
+    }
     this.setState({
       score: this.state.score + 10,
+      rounds: this.state.rounds - 1,
     });
   };
   nextCircle = () => {
+    if (this.state.rounds >= 5) {
+      this.stopHandler();
+      return;
+    }
     let nextActive;
     do {
       nextActive = getRndInteger(1, 4);
     } while (nextActive === this.state.current);
     this.setState({
       current: nextActive,
+      pace: this.state.pace * 0.95,
+      rounds: this.state.rounds + 1,
     });
+    console.log("rounds", this.state.rounds);
     console.log("active circle is ", this.state.current);
-    this.pace *= 0.95;
-    this.timer = setTimeout(this.nextCircle, this.pace);
+    this.timer = setTimeout(this.nextCircle, this.state.pace);
   };
   startHandler = () => {
     this.nextCircle();
+    this.setState({
+      didGameStart: true,
+    });
   };
   stopHandler = () => {
     this.setState({
       gameOver: true,
+      didGameStart: false,
     });
     clearTimeout(this.timer);
+  };
+  closeHandler = () => {
+    this.setState({
+      gameOver: false,
+      score: 0,
+      current: 0,
+      pace: 1500,
+      rounds: 0,
+    });
   };
   render() {
     return (
@@ -54,14 +82,21 @@ class App extends Component {
                 key={c.id}
                 id={c.id}
                 color={c.color}
-                click={this.clickHandler}
+                click={() => this.clickHandler(c.id)}
                 active={this.state.current === c.id}
               />
             ))}
           </div>
-          {this.state.gameOver && <GameOverPopUp score={this.state.score} />}
+          {this.state.gameOver && (
+            <GameOverPopUp score={this.state.score} close={this.closeHandler} />
+          )}
           <div className="buttons">
-            <button onClick={this.startHandler}>Start</button>
+            <button
+              disabled={this.state.didGameStart}
+              onClick={this.startHandler}
+            >
+              Start
+            </button>
             <button onClick={this.stopHandler}>Stop</button>
           </div>
         </div>
--- src/GameOverPopUp.js
+++ src/GameOverPopUp.js
@@ -1,14 +1,12 @@
 import React from "react";
-const closeHandler = () => {
-  window.location.reload();
-};
+
 const GameOverPopUp = (props) => {
   return (
     <div className="overlay">
       <div className="popUpDiv">
         <h1>Game over</h1>
         <p>Your score was: {props.score}</p>
-        <button onClick={closeHandler}>X</button>
+        <button onClick={props.close}>X</button>
       </div>
     </div>
   );
